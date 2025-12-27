5c621190d55f75db86a2f0e0c8e5a55ede3f7452
Gabriel Hanich <68582067+gabriel-hanich@users.noreply.github.com>
2025-12-27 18:32:33 +1100

    Program Constants

--- drone-firmware/Project Stella/docs/hardwareSpecs.md
+++ drone-firmware/Project Stella/docs/hardwareSpecs.md
@@ -1,2 +1,2 @@
 Board: ESP32-DevkitC-32UE
-IMU: BNO086
+IMU: SparkFun VR IMU Breakout - BNO086 [link](https://www.sparkfun.com/sparkfun-vr-imu-breakout-bno086-qwiic.html#content-documentation)
---
+++ drone-firmware/Project Stella/src/ProgramConstants.h
@@ -0,0 +1,20 @@
+#ifndef PROGRAMCONSTANTS_H
+#define PROGRAMCONSTANTS_H
+
+#include <string>
+
+using namespace std;
+
+/*
+This struct contains a bunch of constant values that won't be 
+changed throughout the code's operation. Note that the SSID
+and password of the wifi network is redacted. Replace these with 
+the values for your given wifi network
+*/
+struct ProgramConstants{
+    const char* ssid = "";
+    const char* password = "";
+    const int port = 80;
+} ;
+
+#endif
